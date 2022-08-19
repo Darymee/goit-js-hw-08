c1dfca65361b4c690f810a4399eb2d1193332958
Darymee <danieledaniele3005@gmail.com>
2022-08-20 01:17:05 +0200

    Add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(+localStorage.getItem(STORAGE_KEY));
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(params) {
+  const seconds = params.seconds;
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
